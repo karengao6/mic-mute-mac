07290209d992a5292257ff43aa292638d7ae25c1
Karen Gao <anxin.gao@gmail.com>
2026-08-24 15:47:49 -0700

    identify which input device macOS considers to be current default

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,42 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+import Foundation
+import CoreAudio
+
+// Gets the audio device that macOS currently considers the default input device.
+// This is normally MacBook's built-in mic, unless you selected another mic.
+func getDefaultInputDevice() -> AudioDeviceID {
+    var deviceID = AudioDeviceID(0)
+
+    var address = AudioObjectPropertyAddress(
+        mSelector: kAudioHardwarePropertyDefaultInputDevice,
+        mScope: kAudioObjectPropertyScopeGlobal,
+        mElement: kAudioObjectPropertyElementMain
+    )
+
+    var size = UInt32(MemoryLayout<AudioDeviceID>.size)
+
+    let status = AudioObjectGetPropertyData(
+        AudioObjectID(kAudioObjectSystemObject),
+        &address,
+        0,
+        nil,
+        &size,
+        &deviceID
+    )
+
+    guard status == noErr else {
+        print("Failed to get default input device.")
+        print("OSStatus: \(status)")
+        exit(1)
+    }
+
+    return deviceID
+}
+
+
+// Find and print the current default input device.
+let deviceID = getDefaultInputDevice()
+
+print("Default input device ID: \(deviceID)")
